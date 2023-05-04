19129e53dc15ea5675b2b3d365f61e41fa772457
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2023-05-04 18:23:08 -0500

    Centralize and query common data with GraphQL

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog.`,
+    author: `Matthew Jutrzonka`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Matthew Jutrzonka`,
+      company: `Blogs Inc.`,
+      address: `PO Box 1234`,
+    },
   },
   plugins: [
     `gatsby-plugin-image`,
