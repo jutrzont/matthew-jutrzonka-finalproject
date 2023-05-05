1b0146d954cba1c3fb273db4eb23d3d0880ea677
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2023-05-04 19:30:32 -0500

    Secure Contentful Keys

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,12 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
     title: `Gatsby Blog`,
@@ -31,8 +26,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `reucmf7yaxea`,
-        accessToken: `J8foEE2aQHADRWabSxf7vIpqFoDQ_VxAeGlQFCIG9DY`,
+        spaceId: `${process.env.SPACE_ID}`,
+        accessToken: `${process.env.ACCESS_TOKEN}`,
       },
     },
     `gatsby-transformer-sharp`,
