8567d9e427e6dec9a450853ae806570759d0c918
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2023-05-05 15:13:44 -0500

    Update Components to use theme

--- blog/src/components/Button/IconButton.js
+++ blog/src/components/Button/IconButton.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { BaseButton } from './BaseButton'
+import React from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
+import { BaseButton } from "./BaseButton"
 
 const StyledButton = styled(BaseButton)`
-
+  ${({ theme, variant }) => theme.variants.iconButton[variant || "primary"]}
 `
 
 export const IconButton = styled(({ icon, ...rest }) => {
-    let clone = React.cloneElement(icon, rest)
-    return <StyledButton as={clone.type} {...rest} className={rest.className} />
-})
+  let clone = React.cloneElement(icon, rest)
+  return <StyledButton as={clone.type} {...rest} className={rest.className} />
+})`
+  //Styles can be added here but not necessary
+  // Main reason this is a styled component is to wrap the styled icon passed in as a prop
+`
 
 IconButton.defaultProps = {
-    size: 24,
+  size: 24,
 }
 
 IconButton.propTypes = {
-    icon: PropTypes.node.isRequired
+  icon: PropTypes.node.isRequired,
+  variant: PropTypes.string,
 }
