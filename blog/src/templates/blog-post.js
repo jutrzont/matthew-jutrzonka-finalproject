269beece10101e70fb1b47c8e557325ae8ef3af3
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2023-05-05 15:26:37 -0500

    Finished Project

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import { H1 } from "../components/Heading"
 
 const BlogPost = ({ data }) => {
-  const { title, body } = data.contentfulBlogPost
+  const { title, body, heroImage } = data.contentfulBlogPost
 
   return (
     <Layout>
+      <GatsbyImage image={heroImage.gatsbyImageData} alt={``} />
       <H1>{title}</H1>
       <div
         dangerouslySetInnerHTML={{ __html: body.childMarkdownRemark.html }}
@@ -27,6 +29,9 @@ export const pageQuery = graphql`
         childMarkdownRemark {
           html
         }
+      }
+      heroImage {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED, width: 960)
       }
     }
   }
