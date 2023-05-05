d19c1c7b33e29aebee567a1139a01be8b905ed22
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2023-05-05 12:23:16 -0500

    Create List Components

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -4,22 +4,23 @@ import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { List, ListItem } from "../components/List"
 import * as styles from "../components/index.module.css"
 
 const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <ul className={styles.list}>
+    <List width={[1, 2 / 3, 7 / 8]} p={2}>
       {data.allContentfulBlogPost.edges.map(edge => (
-        <li key={edge.node.id}>
+        <ListItem p={3} key={edge.node.id}>
           <Link to={edge.node.slug}>{edge.node.title}</Link>
           <div>
-            <GatsbyImage image={edge.node.heroImage.gatsbyImageData} alt={``} />
+            <GatsbyImage image={edge.node.heroImage.gatsbyImageData} />
           </div>
           <div>{edge.node.body.childMarkdownRemark.excerpt}</div>
-        </li>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   </Layout>
 )
 
@@ -44,7 +45,7 @@ export const query = graphql`
             gatsbyImageData(
               layout: CONSTRAINED
               placeholder: BLURRED
-              width: 300
+              width: 600
             )
           }
         }
