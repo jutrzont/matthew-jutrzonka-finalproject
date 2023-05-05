2292896e9f61ae52721c2be47e8ca735e90c4bb9
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2023-05-05 12:32:05 -0500

    Create Semantic Element Components

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from "styled-components"
+import { H1 } from "../Heading"
 
 const Outer = styled.header`
   background: ${props => props.theme.header.backgroundColor};
@@ -11,10 +12,6 @@ const Outer = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-`
-
-const H1 = styled.h1`
-  margin: 0px;
 `
 
 const StyledLink = styled(Link)`
